1a0eba0d3e1a311ba7964a5335efe35baecb4d29
Sean Kelliher <sbkelliher@icloud.com>
2020-11-12 13:05:11 -0500

    Modify notes only. No changes to code.

--- mouseclick.js
+++ mouseclick.js
@@ -20,15 +20,14 @@ function mouseClick() {
         const targetId = event.target.id;
         document.getElementById(targetId).setAttribute("fill", "#F2CB05");
 
-        //Retrieve spans where census data goes.
-        //State's name and numbers.
+        //Retrieve elements where state name and census counts go.
         const ckState = document.getElementById("ck-state");
         const ck1980 = document.getElementById("ck-1980");
         const ck1990 = document.getElementById("ck-1990");
         const ck2000 = document.getElementById("ck-2000");
         const ck2010 = document.getElementById("ck-2010");
 
-        //State's percentage change.
+        //Retrieve elements where percentage of change goes.
         const ck1990Sup = document.getElementById("ck-1990-sup");
         const ck2000Sup = document.getElementById("ck-2000-sup");
         const ck2010Sup = document.getElementById("ck-2010-sup");
@@ -36,16 +35,15 @@ function mouseClick() {
         //Retrieve clicked state from "allStates" object.
         const chosenState = allStates[targetId];
 
-        //Put census data in spans.
+        //Place state name and census counts into elements.
         //toLocalString() adds commas to numbers > three digits.
-        //State's name and numbers.
         ckState.textContent = chosenState.name;
         ck1980.textContent = chosenState.c1980.toLocaleString();
         ck1990.textContent = chosenState.c1990.toLocaleString();
         ck2000.textContent = chosenState.c2000.toLocaleString();
         ck2010.textContent = chosenState.c2010.toLocaleString();
 
-        //State's percentage change.
+        //Place percentage of change into elements.
         ck1990Sup.textContent = `${chosenState.diff1980to1990()}%`;
         ck2000Sup.textContent = `${chosenState.diff1990to2000()}%`;
         ck2010Sup.textContent = `${chosenState.diff2000to2010()}%`;
