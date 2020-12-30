b12b65612d51fe50ded91aef7303535f377051e8
Sean Kelliher <sbkelliher@icloud.com>
2020-12-30 13:18:11 -0500

    Overhauling JS. Removing these files: 'defaultdc.js mouseout.js setstates.js mouseover.js states.js app.js mouseclick.js app.js'. Replacements coming next.

--- app.js
+++
@@ -1,9 +0,0 @@
-import {mouseOver} from "./mouseover.js";
-import {mouseOut} from "./mouseout.js";
-import {mouseClick} from "./mouseclick.js";
-import {defaultDc} from "./defaultdc.js";
-
-mouseOver(); //Handles when users mouse-over states.
-mouseOut(); //Handles when users mouse-out of states.
-mouseClick(); //Handles when users click on states.
-defaultDc(); //Highlights Washington, DC when page loads.
