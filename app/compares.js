6c5586ff08350db600320aa75650665a487bf1e7
Sean Kelliher <sbkelliher@icloud.com>
2021-05-14 13:53:47 -0400

    Add variables for 2020.

--- app/compares.js
+++ app/compares.js
@@ -9,11 +9,13 @@ function showCompares(etid) {
     const c1990 = document.getElementById("c1990");
     const c2000 = document.getElementById("c2000");
     const c2010 = document.getElementById("c2010");
+    const c2020 = document.getElementById("c2020");
 
     //Gather elements where percentage changes go.
     const c1990Dif = document.getElementById("c1990-dif");
     const c2000Dif = document.getElementById("c2000-dif");
     const c2010Dif = document.getElementById("c2010-dif");
+    const c2020Dif = document.getElementById("c2020-dif");
 
     //Retrieve clicked state from "allStates" object.
     const stateName = allStates[etid].name;
@@ -21,6 +23,7 @@ function showCompares(etid) {
     const c1990Data = allStates[etid].c1990;
     const c2000Data = allStates[etid].c2000;
     const c2010Data = allStates[etid].c2010;
+    const c2020Data = allStates[etid].c2020;
 
     //Calculate percentage change between census counts.
     function getChange(count1, count2) {
@@ -41,11 +44,13 @@ function showCompares(etid) {
     c1990.textContent = c1990Data.toLocaleString();
     c2000.textContent = c2000Data.toLocaleString();
     c2010.textContent = c2010Data.toLocaleString();
+    c2020.textContent = c2020Data.toLocaleString();
 
     //Place percentage of change into elements.
     c1990Dif.textContent = `${getChange(c1980Data, c1990Data)}%`;
     c2000Dif.textContent = `${getChange(c1990Data, c2000Data)}%`;
     c2010Dif.textContent = `${getChange(c2000Data, c2010Data)}%`;
+    c2020Dif.textContent = `${getChange(c2010Data, c2020Data)}%`;
 }
 
 export {showCompares};
