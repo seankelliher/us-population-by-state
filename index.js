22325cade684406593ae6fee5fdf5d9b06406e2f
Sean Kelliher <sbkelliher@icloud.com>
2022-11-27 11:30:49 -0500

    Addd 'keyup' event listener. Accessibility req.

--- index.js
+++ index.js
@@ -2,33 +2,41 @@ import {standardizeFill} from "./app/standards.js";
 import {mouseOver, mouseOut, mouseClick} from "./app/highlights.js";
 import {showCompares} from "./app/compares.js";
 
-//After DOM loads...
+// After DOM loads...
 document.addEventListener("DOMContentLoaded", function () {
     "use strict";
 
-    //Set the fill color for all states.
+    // Set the fill color for all states.
     standardizeFill();
 
-    //Highlight California. Show its data.
+    // Highlight California. Show its data.
     mouseClick("california");
     showCompares("california");
 
-    //Get "outlines" id (this houses all "states").
+    // Get "outlines" id (this houses all "states").
     const outlines = document.getElementById("outlines");
 
-    //Monitor states for mouse overs.
+    // Monitor states for mouse overs.
     outlines.addEventListener("mouseover", function (event) {
         mouseOver(event.target.id);
     });
 
-    //Monitor states for mouse outs.
+    // Monitor states for mouse outs.
     outlines.addEventListener("mouseout", function (event) {
         mouseOut(event.target.id);
     });
 
-    //Monitor states for clicks.
+    // Monitor states for clicks.
     outlines.addEventListener("click", function (event) {
         mouseClick(event.target.id);
         showCompares(event.target.id);
+    });
+
+    // Monitor states for keyups.
+    outlines.addEventListener("keyup", function (event) {
+        if (event.code === "Enter") {
+            mouseClick(event.target.id);
+            showCompares(event.target.id);
+        }
     });
 });
